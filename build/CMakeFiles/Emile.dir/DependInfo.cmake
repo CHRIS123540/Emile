82e25f7f27638f3deed47047235cf9474f823a54
CHRIS123540 <7355143682qq.com>
2023-11-17 11:16:26 +0800

    first commit

---
+++ build/CMakeFiles/Emile.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/hlx/hlx/Emiles/src/main.c" "/home/hlx/hlx/Emiles/build/CMakeFiles/Emile.dir/src/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/opt/mellanox/dpdk/include/dpdk"
+  "/opt/mellanox/dpdk/include/dpdk/../x86_64-linux-gnu/dpdk"
+  "/usr/include/libnl3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
