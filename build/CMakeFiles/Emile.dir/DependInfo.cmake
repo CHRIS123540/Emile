3938d38f20248905a55e38fec65b63244d507b8d
CHRIS123540 <7355143682qq.com>
2023-11-20 01:30:39 +0800

    third commit

--- build/CMakeFiles/Emile.dir/DependInfo.cmake
+++ build/CMakeFiles/Emile.dir/DependInfo.cmake
@@ -10,8 +10,6 @@ set(CMAKE_C_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
-  "/opt/mellanox/dpdk/include/dpdk"
-  "/opt/mellanox/dpdk/include/dpdk/../x86_64-linux-gnu/dpdk"
   "/usr/include/libnl3"
   )
 
