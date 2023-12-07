b0bc63aa3a91468b1652f03226828129dab6a92f
Hu Carl <64949823+CHRIS123540@users.noreply.github.com>
2023-12-07 12:19:14 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -9,14 +9,16 @@
 #include <rte_timer.h>
 #include <rte_hash.h>
 #include <rte_jhash.h>
-
+#include <rte_mbuf_dyn.h>
+#include <rte_malloc.h>
 
 #define RX_RING_SIZE 1024
 #define TX_RING_SIZE 1024
-
+#define RING_BUF_SIZE 65536
 #define NUM_MBUFS 8191
 #define MBUF_CACHE_SIZE 250
 #define BURST_SIZE 32
+#define FLOW_NUM 2000
 
 #define PRINT_IP_ADDR(ip_addr) printf("IP: %d.%d.%d.%d\n", \
     (int)((ip_addr) >> 24) & 0xFF, \
@@ -32,10 +34,32 @@
 #define PRINT_PORT(port)\
 	printf("PORT: %" PRIu16 "\n", port);
 
+#define PRINT(ipv4_tuple,eth_hdr)\
+	PRINT_PORT_MAC(eth_hdr->src_addr);\
+	PRINT_PORT_MAC(eth_hdr->dst_addr);\
+	PRINT_IP_ADDR(ipv4_tuple.src_addr);\
+	PRINT_IP_ADDR(ipv4_tuple.dst_addr);\
+	PRINT_PORT(ipv4_tuple.sport);\
+	PRINT_PORT(ipv4_tuple.dport)	
+
 static uint64_t Interval;
 uint64_t total = 0;
 uint64_t total_last = 0;
+
+uint64_t total_detect = 0;
+uint64_t total_detect_last = 0;
 uint64_t Flow_num = 2000000;
+struct rte_hash *flow_tables;
+struct rte_ring *ring;
+//作为特征桶
+
+
+typedef struct flow
+{
+    int label;                      	/* 0: normal, 1: mining */
+} flow;
+
+flow *buckets;
 
 typedef struct v4_tuple
 {
@@ -50,7 +74,7 @@ typedef struct v4_tuple
 	};
 } v4_tuple;
 
-static struct rte_hash *create_hash_table(const char *tablename, uint32_t entry_num, uint32_t init_num)
+static struct rte_hash *create_hash_table(const char *tablename, uint32_t entry_num)
 {
 	struct rte_hash *hash_table;
 	struct rte_hash_parameters hash_params = {0};
@@ -58,7 +82,7 @@ static struct rte_hash *create_hash_table(const char *tablename, uint32_t entry_
 	hash_params.entries = entry_num;
 	hash_params.key_len = sizeof(v4_tuple);
 	hash_params.hash_func = rte_jhash;
-	hash_params.hash_func_init_val = init_num;
+	hash_params.hash_func_init_val = 0;
 
 	hash_params.name = tablename;
 	hash_params.socket_id = rte_socket_id();
@@ -77,7 +101,7 @@ static struct rte_hash *create_hash_table(const char *tablename, uint32_t entry_
 		hash_table = rte_hash_create(&hash_params);
 		if (!hash_table)
 		{
-			printf("creay failed\n");
+			printf("creat failed\n");
 			exit(0);
 		}
 	}
@@ -86,12 +110,23 @@ static struct rte_hash *create_hash_table(const char *tablename, uint32_t entry_
 }
 
 
+static const struct rte_eth_conf port_conf_default = {
+	.rxmode = {
+		.mq_mode = RTE_ETH_MQ_RX_RSS,
+	},
+	.rx_adv_conf = {
+		.rss_conf = {
+			.rss_hf = RTE_ETH_RSS_IP | RTE_ETH_RSS_UDP |
+				RTE_ETH_RSS_TCP,
+		}
+	},
+};
 
 //启动端口
 static inline int
 port_init(uint16_t port, struct rte_mempool *mbuf_pool)
 {
-	struct rte_eth_conf port_conf;
+	struct rte_eth_conf port_conf = port_conf_default;
 	const uint16_t rx_rings = 1, tx_rings = 1;
 	uint16_t nb_rxd = RX_RING_SIZE;
 	uint16_t nb_txd = TX_RING_SIZE;
@@ -166,18 +201,81 @@ port_init(uint16_t port, struct rte_mempool *mbuf_pool)
 	return 0;
 }
 
-
 static int
-lcore_main(__rte_unused void *arg)
+lcore_detect(__rte_unused void *arg)
 {
-	uint16_t port;
+	uint16_t port = rte_lcore_id();
+	printf("port DETECT%d\n\n\n\n\n",port);
     struct rte_ipv4_hdr *ipv4;
     struct rte_tcp_hdr *tcp;
     struct rte_udp_hdr *udp;
 	v4_tuple ipv4_tuple;
 	struct rte_ether_hdr *eth_hdr;                
 
+	flow *CV[10] ;
+
+
+	/* Main work of application loop. 8< */
+	while(1){
+		//rte_delay_ms(100);
+		int NUM = rte_ring_sc_dequeue_burst(ring, (void **)CV, 10, NULL);
+		if(NUM > 0){
+			
+			for(int i =0;i < NUM; i++)
+			{
+//				printf("CV %d\n",CV[i]->label);
+				total_detect = total_detect + 1;
+			//printf("CV %d\n",*CV[0]);			
+			//printf("core 2\n");
+			}
+		}
+	}
+	/* >8 End of loop. */
+	return 0;
+}
+
+static uint32_t hash_match(struct rte_hash *flow_tables, v4_tuple ipv4_tuple)
+{
 	
+	int flowid;
+	int ret = rte_hash_lookup(flow_tables, (void const *)&ipv4_tuple);
+
+	int C = 1;
+	int *cv = &C;
+
+
+
+		if (ret == -ENOENT)
+		{
+			flowid = rte_hash_add_key(flow_tables, (void const *)&ipv4_tuple);
+//			printf("NEW TABLE %d\n",flowid);
+		}
+		else if (ret == -EINVAL)
+		{
+//			printf("Error!\n");
+		}
+		else
+		{
+			
+			buckets[ret].label = ret;
+			flow *cnn = &buckets[ret]; 
+			int success = rte_ring_sp_enqueue_burst(ring, (void **)&cnn, 1, NULL);	
+//			printf("success %d\n\n\n",success);
+//			printf("MATCH Ret %d!\n",ret);
+		}
+
+}
+
+static int
+lcore_main(__rte_unused void *arg)
+{
+	uint16_t port = rte_lcore_id();
+	printf("port %d\n\n\n\n\n",port);
+    struct rte_ipv4_hdr *ipv4;
+    struct rte_tcp_hdr *tcp;
+    struct rte_udp_hdr *udp;
+	v4_tuple ipv4_tuple;
+	struct rte_ether_hdr *eth_hdr;                
 	RTE_ETH_FOREACH_DEV(port)
 		if (rte_eth_dev_socket_id(port) >= 0 &&
 				rte_eth_dev_socket_id(port) !=
@@ -186,8 +284,27 @@ lcore_main(__rte_unused void *arg)
 					"polling thread.\n\tPerformance will "
 					"not be optimal.\n", port);
 
-	printf("\nCore %u forwarding packets. [Ctrl+C to quit]\n",rte_lcore_id());
+	
+	
+	ring = rte_ring_create("Emile", RING_BUF_SIZE, rte_socket_id(), RING_F_MP_RTS_ENQ | RING_F_SC_DEQ);
+    
+	int C = 1;
+	
+	int *cv = &C;
+	rte_ring_sp_enqueue_burst(ring, (void **)&cv, 1, NULL);	
+    
+	C = 2;
+	cv = &C;
+	rte_ring_sp_enqueue_burst(ring, (void **)&cv, 1, NULL);		
+	
+	printf("\nCore %u forwarding packets%d %d. [Ctrl+C to quit]\n",rte_lcore_id(),rte_ring_count(ring),rte_ring_get_capacity(ring));
+	
+	int *CV[2] ;
+	rte_ring_sc_dequeue_burst(ring, (void **)CV, 2, NULL);
+	printf("CV %d\n",*CV[0]);
+	printf("CV %d\n",*CV[1]);
 
+	
 	/* Main work of application loop. 8< */
 	while(1){
 		RTE_ETH_FOREACH_DEV(port) {
@@ -200,40 +317,47 @@ lcore_main(__rte_unused void *arg)
 			if (unlikely(nb_rx == 0))
 				continue;
 
-			/* Send burst of TX packets, to second port of pair. */
-			//const uint16_t nb_tx = rte_eth_tx_burst(port, 0,bufs, nb_rx);
 			if(nb_rx > 0)
 			{
-		    
 			    total += nb_rx;				
 			//	printf("Total %ld\n",total);
 			}
-
 			/*这一块用于释放收包*/
-			/**/
 			uint16_t buf;
+			int flowid;
 			for (buf = 0; buf < nb_rx; buf++){
 
+
+				uint32_t usr_value = bufs[buf]->hash.rss;
+//            	printf("Packet usr value: %u\n", usr_value);
+
 				ipv4 = (struct rte_ipv4_hdr *)(rte_pktmbuf_mtod(bufs[buf], struct rte_ether_hdr *) + 1);
-				ipv4_tuple.src_addr = rte_be_to_cpu_32(ipv4->src_addr);
-				ipv4_tuple.dst_addr = rte_be_to_cpu_32(ipv4->dst_addr);
-				eth_hdr = rte_pktmbuf_mtod(bufs[buf], struct rte_ether_hdr *);
+                ipv4_tuple.src_addr = rte_be_to_cpu_32(ipv4->src_addr);
+                ipv4_tuple.dst_addr = rte_be_to_cpu_32(ipv4->dst_addr);
+				//eth_hdr = rte_pktmbuf_mtod(bufs[buf], struct rte_ether_hdr *);
 			
-
-				if (ipv4->next_proto_id == 6)
-				{
-					tcp = (struct rte_tcp_hdr *)((unsigned char *)ipv4 + sizeof(struct rte_ipv4_hdr));
-					ipv4_tuple.sport = rte_be_to_cpu_16(tcp->src_port);
-					ipv4_tuple.dport = rte_be_to_cpu_16(tcp->dst_port);
+				/* only process TCP flows */
+                if (ipv4->next_proto_id == 6)
+                {
+                    tcp = (struct rte_tcp_hdr *)((unsigned char *)ipv4 + sizeof(struct rte_ipv4_hdr));
+                    ipv4_tuple.sport = rte_be_to_cpu_16(tcp->src_port);
+                    ipv4_tuple.dport = rte_be_to_cpu_16(tcp->dst_port);
 					
-				}
-				else if (ipv4->next_proto_id == 17)
-				{
-					udp = (struct rte_udp_hdr *)((unsigned char *)ipv4 + sizeof(struct rte_ipv4_hdr));
-					ipv4_tuple.sport = rte_be_to_cpu_16(udp->src_port);
-					ipv4_tuple.dport = rte_be_to_cpu_16(udp->dst_port);
+					//源/目的mac->源/目的ip->源/目的port
+//					PRINT(ipv4_tuple,eth_hdr);
+//					printf("lenth %u\n",rte_pktmbuf_pkt_len(bufs[buf]));
+					
+					int ret = hash_match(flow_tables, ipv4_tuple);
 
-				}	
+
+                }
+                else if (ipv4->next_proto_id == 17)
+                {
+                    udp = (struct rte_udp_hdr *)((unsigned char *)ipv4 + sizeof(struct rte_ipv4_hdr));
+                    ipv4_tuple.sport = rte_be_to_cpu_16(udp->src_port);
+                    ipv4_tuple.dport = rte_be_to_cpu_16(udp->dst_port);
+
+                }	
 				
 				rte_pktmbuf_free(bufs[buf]);
 				}
@@ -253,8 +377,10 @@ static void stats_display()
 
 	/* Clear screen and move to top left */
 	printf("%s%s", clr, top_left);
-	printf("Total %ld\n",total - total_last);
+	printf("Total %lu\n",total - total_last);
+	printf("Total_detect %lu\n",total_detect - total_detect_last);
 	total_last = total;
+	total_detect_last = total_detect;
 }
 
 // 定时器回调函数
@@ -286,6 +412,8 @@ main(int argc, char *argv[])
 		MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
 	/* >8 End of allocating mempool to hold mbuf. */
 
+	buckets = (flow *)rte_malloc("bucket", FLOW_NUM * sizeof(flow), RTE_CACHE_LINE_SIZE);
+
 	if (mbuf_pool == NULL)
 		rte_exit(EXIT_FAILURE, "Cannot create mbuf pool\n");
 
@@ -296,6 +424,11 @@ main(int argc, char *argv[])
 					portid);
 	/* >8 End of initializing all ports. */
 
+	/*创建哈希表*/
+	char tbl_name[RTE_MEMPOOL_NAMESIZE];
+	snprintf(tbl_name, RTE_MEMPOOL_NAMESIZE, "Emile");	
+	flow_tables = create_hash_table(tbl_name, Flow_num);
+	
 	printf("\n%d lcores used.\n",rte_lcore_count());
 	
 	// 初始化定时器子系统
@@ -311,7 +444,7 @@ main(int argc, char *argv[])
     rte_timer_reset(&timer, hz, PERIODICAL, rte_lcore_id(), timer_callback, NULL);
 
 	rte_eal_remote_launch(lcore_main, NULL, 1);
-
+	rte_eal_remote_launch(lcore_detect, NULL, 2);
 /*此处预计放置打印的内容*/
     while (1) {
         const char *data = "1\n";
